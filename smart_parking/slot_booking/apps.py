12b785ff2b9191a10ff0bea006f2f36abac5e254
DinithShamika <dinithsh256@gmail.com>
2025-07-29 22:04:34 +0530

    Update apps.py

--- smart_parking/slot_booking/apps.py
+++ smart_parking/slot_booking/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SlotBookingConfig(AppConfig):
+class BookingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'slot_booking'
+    name = 'booking'
